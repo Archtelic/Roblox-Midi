c271e167efec8c1cfb228a4a6c99a8ef488b55a1
Archtelic <60186647+Archtelic@users.noreply.github.com>
2023-05-04 12:37:07 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -5,13 +5,14 @@ fn main(){
     let mut keyboard_map = HashMap::new();
     let keyboard = "1234567890qwertyuiopasdfghjklzxcvbnm";
     let mut temp: u8 = 2;
+    const OFFSET: u8 = 34;
     for i in keyboard.chars(){
-        keyboard_map.insert(temp + 34, i.to_string());
+        keyboard_map.insert(temp + OFFSET, i.to_string());
 
         if [2, 4, 7, 9, 11].contains(&(temp % 12)) {
             
             temp = temp + 1;
-            keyboard_map.insert(temp + 34, format!("shift + {}", i.to_string()));
+            keyboard_map.insert(temp + OFFSET, format!("shift + {}", i.to_string()));
         }
         temp = temp + 1;
     }
